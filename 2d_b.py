f99913a16cb5ede8291679b1a06cc9455b3de7e7
mrlj <ruanquanyuan@outlook.com>
2020-08-21 10:22:54 +0800

    增加了斜面波

--- 2d_b.py
+++ 2d_b.py
@@ -1,6 +1,5 @@
 import numpy as np
-import matplotlib.pyplot  as plt
-from math import *
+import matplotlib.pyplot as plt
 
 # to get the distance of wave
 # y = kx + b
@@ -14,7 +13,6 @@ class F:
     def distance(self, x, y):
         return np.abs((self.a*x + self.b*y+self.c)/(np.sqrt(self.b**2+self.a**2)))
 
-
 # become real axic
 def axic(x, y):
     _x = x * _w / _N
@@ -24,6 +22,7 @@ def axic(x, y):
 
 # var
 L = 9
+W = 9
 
 # const var
 _pi = 3.1415926
@@ -34,24 +33,25 @@ _v = 40000
 _lambda = 343 / 40000
 
 # degree
-_N, _M = 200, 200
+_N, _M = 20, 20
 
 # create zero array
 array = np.zeros((_N, _M))
 
 # length
 _l = L * _lambda / 2
-_w = L * _lambda / 2
+_w = W * _lambda / 2
 
 f1 = F(0, 1, 0)
 f2 = F(0, 1, -_l)
+f3 = F(1, 0, 0)
+f4 = F(1, 0, -_w)
 
-for i in range(0, _N):
-    for j in range(0, _M):
+for i in range(_N):
+    for j in range(_M):
         _x, _y = axic(i, j)
-        array[j][i] = _pi * (f1.distance(_x, _y)+_lambda/2-f2.distance(_x, _y)) / _lambda
-
-array = np.cos(array)
+        array[j][i] += np.cos(_pi * ((f1.distance(_x, _y)+_lambda/2-f2.distance(_x, _y))/_lambda))
+        array[j][i] += np.cos(_pi * ((f3.distance(_x, _y)+_lambda/2-f4.distance(_x, _y))/_lambda))
 
 array = np.abs(array)
 
